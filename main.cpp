ceb252dccd94da875e45f9ba094910ba201d49fc
Christiany Cavalcante <>
2021-04-17 22:35:06 +0000

    melhoria subtração

--- main.cpp
+++ main.cpp
@@ -39,47 +39,40 @@ string soma(string numero1, string numero2){
     return result;
 }
 
+//subtração utilizando método de complemento de dois
 string subtracao(string numero1, string numero2){
-    string result = ""; 
-    int s = 0;         
-    int tam1 = numero1.size() - 1, tam2 = numero2.size() - 1;
-    while (tam1 >= 0 || tam2 >= 0)
-    {
-        
-      if(tam1 >= 0){
-        validarBinario(numero1[tam1]);
-        s += numero1[tam1]- '0';
-      }
+    numero1 = "0"+numero1;
+    numero2 = "0"+numero2;
+    string sinal = "";
 
-      if(tam2 >= 0){
-        validarBinario(numero2[tam2]);
-        s -= numero2[tam2]- '0';
-      }
+  //se o numero2 for maior, o resultado será negativo.
+  //Como o metodo de complemento de dois não suporta essa operação, inverto os valores e acrescento o sinal ao resultado final;
+    if(numero2 > numero1){
+      string aux = numero1;
+      numero1 = numero2;
+      numero2 = aux;
+      sinal = "-";
+    }
 
+    string complemento1 = "";
+    for(int i = 0; i<numero2.size(); i++){      
+      complemento1 = complemento1 + (numero2[i] == '0' ? '1' : '0');
+    }
     
 
-    if(s==-1){
-      
-      if(tam1 == 0 && tam2 == 0){
-        result = "-1" + result;
-      }else{
-        result = "1" + result;
-      }
-      
-    }else{    
-      
-      result = char(s + '0') + result;
-      s = 0;
-    }
-  
-  
-  
+    string completemento2 = soma(complemento1, "1");
     
-        
-      tam1--; tam2--;
+
+    string resultadoPacial = soma(numero1,completemento2);
+    cout<<"resultadoPacial"<<resultadoPacial<<endl;
+
+    if(resultadoPacial.length()>1){
+      return  sinal + resultadoPacial.substr (1,resultadoPacial.length()); 
+    }else{
+      return  sinal + resultadoPacial; 
     }
-    return result;
-    }
+
+}
 
 string divisao(string numero1, string numero2){
    return  "divisao\n";
