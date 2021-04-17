706216e3bb1fa7f705e20726dd0b1d47e4b9dc1b
Christiany Cavalcante <>
2021-04-17 17:57:08 +0000

    Criação estrutura inicial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+
+using namespace std;
+
+string soma(string numero1, string numero2){
+   return "soma\n";   
 }
+
+string subtracao(string numero1, string numero2){
+   return  "subtracao\n";
+}
+
+string divisao(string numero1, string numero2){
+   return  "divisao\n";
+}
+
+string multicacao(string numero1, string numero2){
+   return  "multicacao\n";
+}
+
+string  modulo(string numero1, string numero2){
+   return  "modulo\n";
+}
+
+
+int main() {
+  char operacao;
+  string numero1;
+  string numero2;
+  string resultado;
+  
+    cin >> operacao;
+    cin >> numero1;
+    cin >> numero2;
+
+    switch(operacao){
+      case '+':
+        resultado = soma(numero1, numero2);
+        break;
+      case '-':
+        resultado = subtracao(numero1, numero2);
+        break;
+      case '/':
+        resultado = divisao(numero1, numero2);
+        break;
+      case '*':
+        resultado = multicacao(numero1, numero2);
+        break;
+      case '%':
+        resultado = modulo(numero1, numero2);
+        break;
+      default:
+      cout << "OPERACAO INVALIDA\n";
+    }
+
+    
+cout << resultado;
+
+}
+
